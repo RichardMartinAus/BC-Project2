2618bbffc93c8febc21fd1fa630beceb8e51979c
Ning <ningzhawb@gmail.com>
2022-08-29 19:38:18 +1000

    added handlebars, css, js and image files

---
+++ public/js/navbar.js
@@ -0,0 +1,15 @@
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+// hamburger on click displays menu
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+})
+
+// navigation item on click closes menu
+document.querySelectorAll(".nav-link").forEach(n => n.
+    addEventListener("click", () => {
+        hamburger.classList.remove("active");
+        navMenu.classList.remove("active");
+    }))
