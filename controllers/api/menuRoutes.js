1b5ec79be0ee57597086b8ab03869d623e8ad107
Anu.Saini <105421965+Anu-Saini@users.noreply.github.com>
2022-08-30 21:45:02 +1000

    updated menuRoute

--- controllers/api/menuRoutes.js
+++ controllers/api/menuRoutes.js
@@ -1,2 +1,33 @@
 // ROUTES FOR MENU
+const router = require('express').Router();
+const Products = require('../../models/Products');
+
+// route to get all productses
+router.get('/', async (req, res) => {
+  const product = await Products.findAll().catch((err) => { 
+      res.json(err);
+    });
+    res.send(product);
+      // const items = product.map((item) => item.get({ plain: true }));
+      // res.send('all', { items });
+    });
+
+// route to get one products
+router.get('/:id', async (req, res) => {
+  try{ 
+      const product = await Products.findByPk(req.params.id);
+      if(!product) {
+          res.status(404).json({message: 'No products with this id!'});
+          return;
+      }
+      const products = product.get({ plain: true });
+      
+      res.send(product)
+//       res.render('Products', products);
+    } catch (err) {
+        res.status(500).json(err);
+    };     
+});
+
+module.exports = router;
 
